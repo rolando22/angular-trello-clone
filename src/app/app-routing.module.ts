9b51299e45f917890de5d1ffe6b5f5c25758c462
rolando22 <rolando2207@gmail.com>
2023-11-26 12:16:19 -0300

    [ADD] página de Boards y componente Nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-  }
+  },
+  {
+    path: 'boards',
+    component: BoardsComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 import { ButtonComponent } from './components/button/button.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ButtonComponent
+    ButtonComponent,
+    BoardsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    OverlayModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
