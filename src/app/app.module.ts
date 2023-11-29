234b1e27db25d651df3dfc7bbde6079cfd6e70e7
rolando22 <rolando2207@gmail.com>
2023-11-28 21:44:03 -0300

    [ADD] página de Scroll y funcionalidad de Virtual Scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DialogModule } from '@angular/cdk/dialog';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BoardsComponent } from './pages/boards/boards.component';
 import { BoardComponent } from './pages/board/board.component';
+import { ScrollComponent } from './pages/scroll/scroll.component';
 import { ButtonComponent } from './components/button/button.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TodoModalComponent } from './components/todo-modal/todo-modal.component';
@@ -21,6 +24,7 @@ import { TodoModalComponent } from './components/todo-modal/todo-modal.component
     LoginComponent,
     BoardsComponent,
     BoardComponent,
+    ScrollComponent,
     ButtonComponent,
     NavComponent,
     TodoModalComponent,
@@ -28,11 +32,13 @@ import { TodoModalComponent } from './components/todo-modal/todo-modal.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     OverlayModule,
     FontAwesomeModule,
     CdkAccordionModule,
     DragDropModule,
     DialogModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
