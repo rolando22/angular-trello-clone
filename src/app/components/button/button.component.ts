6ef98925d5396318cd3fd6d95726e0967a310b1e
rolando22 <rolando2207@gmail.com>
2023-11-29 16:27:23 -0300

    [UPDATE] mapColors del componente Button y cierre de Overlay al clickear fuera de la ventana

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,17 +7,18 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
 
   @Input() type: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color: 'primary' | 'success' | 'danger' | 'sky' | 'gray-light' = 'primary';
+  @Input() color: keyof typeof this.mapColors = 'primary';
+
+  mapColors = {
+    primary: 'text-white bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
+    success: 'text-white bg-success-700 hover:bg-success-800 focus:ring-success-300',
+    danger: 'text-white bg-danger-700 hover:bg-danger-800 focus:ring-danger-300',
+    sky: 'text-white bg-sky-700 hover:bg-sky-800 focus:ring-sky-300',
+    'gray-light': 'text-gray-700 text-left bg-gray-200 hover:bg-gray-500 focus:ring-gray-50',
+  };
 
   get colors() {
-    const colors: { [key: string]: string } = {
-      primary: 'text-white bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
-      success: 'text-white bg-success-700 hover:bg-success-800 focus:ring-success-300',
-      danger: 'text-white bg-danger-700 hover:bg-danger-800 focus:ring-danger-300',
-      sky: 'text-white bg-sky-700 hover:bg-sky-800 focus:ring-sky-300',
-      'gray-light': 'text-gray-700 text-left bg-gray-200 hover:bg-gray-500 focus:ring-gray-50',
-    };
-    return colors[this.color];
+    return this.mapColors[this.color];
   }
 
 }
