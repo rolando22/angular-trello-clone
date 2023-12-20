1b62f0d94d0348a5a22255258355b42c6cec57e4
rolando22 <rolando2207@gmail.com>
2023-12-19 23:55:44 -0300

    [UPDATE] Pages Profile

--- src/app/modules/profile/pages/profile/profile.component.ts
+++ src/app/modules/profile/pages/profile/profile.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '@services/auth.service';
+import { User } from '@models/user.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  user: User | null = null;
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user$
+      .subscribe(user => this.user = user);
+  }
 
 }
